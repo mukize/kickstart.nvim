278b91c4b634d0e38713337a3db0a6130aded3c6
Mukize <patrickmukize@gmail.com>
2026-04-22 16:41:07 +0200

    markdown: enable treesitter

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,5 +1,6 @@
+vim.treesitter.start()
 vim.opt_local.spell = true
--- vim.opt_local.conceallevel = 2
+vim.opt_local.conceallevel = 2
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
--- vim.cmd.colorscheme 'catppuccin-frappe'
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
